f9d0d4f6ab8ace3817bf10b76f53ee416693a5db
Vincentvelasco22 <160803770+Vincentvelasco22@users.noreply.github.com>
2024-02-22 08:49:48 +0700

    Update streamlit_app.py
    typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-steamlit.multiselect("Pick fruit: " list(my_fruit_list.index))
+steamlit.multiselect("Pick fruit: ", list(my_fruit_list.index))
 
 streamlit.dataframe(my_fruit_list)
